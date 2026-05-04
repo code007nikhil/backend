e66b84ddc245eca18b6e5ccf8c65f2b1d26c89c5
code007nikhil <thakurniks23943@gmail.com>
2026-05-04 12:37:03 +0530

    updates

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
-import "dotenv/config.js";
+import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
+dotenv.config();
 
 const PORT = process.env.PORT;
+const mongoURI = process.env.MONGO_URI;
+
 connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("PORT:", process.env.PORT);
+  console.log("MONGO:", mongoURI);
 });
