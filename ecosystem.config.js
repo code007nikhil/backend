ff92c6cd0225e69f508cfb10da3c33aa0b0d1563
code007nikhil <thakurniks23943@gmail.com>
2026-05-04 12:41:02 +0530

    updates

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "transport-management",
+      script: "src/server.js",
+      cwd: "/root/backend",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        PORT: 5001,
+        MONGODB_URI: "mongodb+srv://thakurniks23943_db_user:LuJEaY8VacpSjYRO@cluster0.w8t8e45.mongodb.net/?appName=Cluster0"
+      }
+    }
+  ]
+};
